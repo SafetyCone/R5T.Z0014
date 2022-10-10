6ffa6e178e6ab82a2d3565680f2e50372d6a6cac
davidcoats <david@getsafetycone.com>
2022-10-10 14:04:32 -0700

    Initial commit.

---
+++ source/R5T.Z0014/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0014
+{
+	/// <summary>
+	/// Example and test repository descriptors (repository names, repository descriptions).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
