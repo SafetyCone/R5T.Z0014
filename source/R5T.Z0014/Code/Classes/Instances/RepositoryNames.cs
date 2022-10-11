aa2bfa6e0df51e7f8e0f3c1498e8145d0be72103
davidcoats <david@getsafetycone.com>
2022-10-10 22:29:21 -0700

    Interim changes.

---
+++ source/R5T.Z0014/Code/Classes/Instances/RepositoryNames.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace R5T.Z0014
+{
+	public class RepositoryNames : IRepositoryNames
+	{
+		#region Infrastructure
+
+	    public static IRepositoryNames Instance { get; } = new RepositoryNames();
+
+	    private RepositoryNames()
+	    {
+        }
+
+	    #endregion
+	}
+}
